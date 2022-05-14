8fa5506a10fd0a23dc9f12661d25b39be80f5119
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-05-14 05:35:33 +0000

    finished p08

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,14 @@
 # Files
+def file():
+  f = open("dataset/mbox-short.txt")
+  count, sum = 0,0
+  for line in f:
+    if line.startswith('X-DSPAM-Confidence'):
+      count += 1
+      x = float(line[20::1])
+      sum += x
+  print('There were', count, 'subject lines in', "dataset/mbox-short.txt" )
+  avg=sum/(count)
+  print("Average spam confidence :", avg)
 
-filename = "dataset/mbox-short.txt"
+file()
