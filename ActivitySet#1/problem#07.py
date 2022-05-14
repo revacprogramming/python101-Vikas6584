53598126829377a8ce47935583ee5105dfff063f
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-05-14 05:29:01 +0000

    finished p07

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,12 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+def str():
+  text = "X-DSPAM-Confidence:    0.8475"
+  string = text.find("0.8475")
+  ans = text[string::1]
+  fans = float(ans)
+  print(fans)
+
+
+str()
+
