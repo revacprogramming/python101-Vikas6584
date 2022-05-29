aba48b1106b32be6624d77ff34b8c13cb5176c32
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-05-29 07:01:22 +0000

    finised set#1, p#12 and set#3, p#1

---
+++ ActivitySet#3/problem#1.py
@@ -0,0 +1,20 @@
+def rect():
+  n=int(input("how many rectangles area do you want?"))
+  i=0
+  while n>i:
+      area()
+      i=i+1
+
+
+def area():
+  x1=eval(input("enter value for x1"))
+  y1=eval(input("enter value for y1"))
+  x2=eval(input("enter value for x2"))
+  y2=eval(input("enter value for y2"))
+  x3=eval(input("enter value for x3"))
+  y3=eval(input("enter value for y3"))
+
+  area=(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y3))
+  print("Area of rectangle with vertices","(",x1,",",y1,")"",""(",x2,",",y2,")"",""(",x3,",",y3,")","is","=",area)
+
+rect()
